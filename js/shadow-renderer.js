e0351f3ec8b2bd65df1686801909ff4657605212
Ishan Khare <ishankhare07@gmail.com>
2015-04-14 16:48:50 +0530

    getBoundingClientRect() working instead of window.getComputedStyle()

--- js/shadow-renderer.js
+++ js/shadow-renderer.js
@@ -2,13 +2,12 @@ function getShadow(element, xPos, yPos) {
     var h_scale = -1/10;
     var v_scale = -1/10;
     
-    pseudoElement = window.getComputedStyle(element);   //get element's computed style
-    
-    var current_left = pseudoElement.getPropertyValue('left');
-    current_left = Number(current_left.slice(0, current_left.length - 2));      //slicing & converting "100px" to 100
+    var element_bounds = element.getBoundingClientRect();
+    var current_left = element_bounds.left;
+    //current_left = Number(current_left.slice(0, current_left.length - 2));      //slicing & converting "100px" to 100
     console.log("current_left : " + current_left);
-    var current_top = pseudoElement.getPropertyValue('top');
-    current_top = Number(current_top.slice(0, current_top.length - 2));
+    var current_top = element_bounds.top;
+    //current_top = Number(current_top.slice(0, current_top.length - 2));
     console.log("current_right : " + current_top);
     
     var new_shh = h_scale * (xPos - current_left);
@@ -32,6 +31,6 @@ function calculate_distance(left, top) {
 
 function renderShadow(element, horizontal, vertical, blur) {
     //console.log("rendering element : " + element);
-    element.style.textShadow = String(horizontal) + "px " + String(vertical) + "px " + String(blur) + "px";
+    element.style.textShadow = String(horizontal) + "px " + String(vertical) + "px " + "0px";
     //console.log("rendered");
 }
